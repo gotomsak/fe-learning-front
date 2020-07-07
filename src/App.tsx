e2b35fa53bc0ca391b66885834a072f6f9bdfbc2
goto <gotomsak@gmail.com>
2020-07-07 18:02:54 +0900

    コメントアウトの削除

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,6 @@ import './App.css';
 import TestContainer from '../src/containers/testContainer'
 import { Switch, Route } from 'react-router';
 import LearningPage from './pages/LearningPage'
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <React.Fragment>
-//           <Switch>
-//             <Route exact path="/" component={LearningPage} />
-//           </Switch>
-//         </React.Fragment>
-//       </div>
-//       // <div className="App">
-//       //   <TestContainer />
-//       // </div>
-//     );
-//   }
-// }
 
 const App=()=>{
   return (
