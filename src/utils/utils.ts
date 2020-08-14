83c03ce2a9c8ee1b9e0b277ad260025a48046765
goto <gotomsak@gmail.com>
2020-08-14 14:54:36 +0900

    getNowTimeStringの実装

---
+++ src/utils/utils.ts
@@ -0,0 +1,13 @@
+
+
+export const getNowTimeString = (): string =>{
+    const date = new Date()
+    return (
+        date.getFullYear().toString() + "-" +
+        ('00' + (date.getMonth()+1).toString()).slice(-2) + "-" +
+        ('00' + date.getDate().toString()).slice(-2) + " " +
+        ('00' + date.getHours().toString()).slice(-2) + ":" +
+        ('00' + date.getMinutes().toString()).slice(-2) + ":"+
+        ('00' + date.getSeconds().toString()).slice(-2)
+    )
+}
