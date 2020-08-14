e52ed5fd4ea111293407bc16dd574ca15b2329f0
goto <gotomsak@gmail.com>
2020-08-14 14:49:35 +0900

    checkAnswerのapiを実装

--- src/apis/backendAPI/interfaces.ts
+++ src/apis/backendAPI/interfaces.ts
@@ -9,3 +9,13 @@ export interface GetQuestionIdsPost{
     solved_ids: number[],
     question_ids: number[]
 }
+
+export interface CheckAnswerPost{
+    question_id: number,
+    user_id: number,
+    other_focus_second:number,
+    user_answer: string,
+    memo_log: string,
+    start_time: string,
+    end_time: string
+}
