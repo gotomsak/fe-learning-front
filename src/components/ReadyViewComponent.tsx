9e22e129fb22fd7190e0c29502b4dc7ea74e2023
goto <gotomsak@gmail.com>
2020-08-18 23:31:53 +0900

    ステート名の変更

--- src/components/ReadyViewComponent.tsx
+++ src/components/ReadyViewComponent.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react'
 
-const ReadyViewComponent: React.FC<{setStart:any}>=({setStart})=>{
+const ReadyViewComponent: React.FC<{setStartCheck:any}>=({setStartCheck})=>{
     const buttonClick =()=>{
-        setStart(true)
+        setStartCheck(true)
     }
     return (
         <div>
