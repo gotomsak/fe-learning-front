a96c309e1b52431442f3df9a72f0ca4bcf2f44bf
goto <gotomsak@gmail.com>
2020-09-16 15:16:58 +0900

    ReadyViewComponentのレイアウト変更

--- src/components/ReadyViewComponent.tsx
+++ src/components/ReadyViewComponent.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Button } from "@material-ui/core";
-
+import "./ReadyViewComponent.css";
 const ReadyViewComponent: React.FC<{ setStartCheck: any }> = ({
     setStartCheck,
 }) => {
@@ -8,11 +8,11 @@ const ReadyViewComponent: React.FC<{ setStartCheck: any }> = ({
         setStartCheck(true);
     };
     return (
-        <div>
+        <div className="ReadyViewContainer">
             <h1>準備は良いですか？</h1>
             <h2>良ければスタートボタンを押してください</h2>
             <h3>10問おきに継続，終了を選べます</h3>
-            <h3>終了後アンケートにお応えください</h3>
+            <h3>終了後アンケートにお答えください</h3>
             <Button color="secondary" onClick={buttonClick}>
                 start
             </Button>
